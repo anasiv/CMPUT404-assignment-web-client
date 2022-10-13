379273cddec3b1e623b0048dcb05f8b34c35fa13
Nasiv <anasiv350@gmail.com>
2022-10-12 20:20:05 -0600

    final work with working GET & POST functions

--- httpclient.py
+++ httpclient.py
@@ -18,9 +18,12 @@
 # Write your own HTTP GET and POST
 # The point is to understand what you have to send and get experience with it
 
+from ast import arg
+from email.quoprimime import header_decode
 import sys
 import socket
 import re
+from urllib import request, response
 # you may use urllib to encode data appropriately
 import urllib.parse
 
@@ -41,13 +44,16 @@ class HTTPClient(object):
         return None
 
     def get_code(self, data):
-        return None
+        return int(data.split()[1])
 
     def get_headers(self,data):
-        return None
+
+        headers = data.split("\r\n\r\n")[0].split("\r\n")[1:]
+
+        return headers
 
     def get_body(self, data):
-        return None
+        return data.split("\r\n\r\n")[1]
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
@@ -67,14 +73,76 @@ class HTTPClient(object):
                 done = not part
         return buffer.decode('utf-8')
 
+    #figure out the hostname, port and path of a given URL
+    #https://docs.python.org/3/library/urllib.parse.html
+    def parse_URL(self, url):
+        parsedurl = urllib.parse.urlparse(url)
+        
+        hostname = parsedurl.hostname
+
+        #if port val is empty then use port 80 as default
+        if parsedurl.port != None:
+            port = parsedurl.port
+        else:
+            port = 80
+
+        #if path stirng val is empty then return a forward slash as default
+        if len(parsedurl.path) != 0:
+            path = parsedurl.path
+        else:
+            path = "/"
+
+        return hostname, port, path
+
     def GET(self, url, args=None):
         code = 500
         body = ""
+
+        #call functon to split the different parts
+        hostname, port, path = self.parse_URL(url)
+
+        #connect, create and send request
+        self.connect(hostname, port)
+
+        #setup headders
+        request = "GET {path} HTTP/1.1\r\n".format(path = path) + "Host: {hostname}:{port}\r\n".format(hostname = hostname, port = port) + "Connection: close\r\n" + "\r\n"
+
+        self.sendall(request)
+
+        #recieve and split request into code and body
+        response = self.recvall(self.socket)
+        code = self.get_code(response)
+        body = self.get_body(response)
+        self.close()
+
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
         code = 500
         body = ""
+
+        #call functon to split the different parts
+        hostname, port, path = self.parse_URL(url)
+
+        #connect, create and send request
+        self.connect(hostname, port)
+
+        if args != None:
+            content = urllib.parse.urlencode(args)
+        else:
+            content = ""
+            
+        #setup headders
+        request = "POST {path} HTTP/1.1\r\n".format(path = path) + "Host: {hostname}:{port}\r\n".format(hostname = hostname, port = port) + "Connection: close\r\n" + "Content-Length: {content_len}\r\n".format(content_len = len(content)) + "\r\n" + "{content}\r\n".format(content = content) + "\r\n"
+
+        self.sendall(request)
+
+        #recieve and split request into code and body
+        response = self.recvall(self.socket)
+        code = self.get_code(response)
+        body = self.get_body(response)
+        self.close()
+
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
